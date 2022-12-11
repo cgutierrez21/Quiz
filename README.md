98b0a9d2f4900850f258fb47c9031f9a38c008eb
Cristian Gutierrez <cristian.j.gutierrez4@gmail.com>
2022-12-10 20:17:53 -0600

    Ask questions until done

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## Day 17
 ---
 1st version
+- Get the questions
+- Ask the questions
+- Stop asking when list is done
--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import data
 import question_model
 import quiz_brain
+import os
+
+os.system("clear")
+
+
+questions = data.question_data
+question_bank =[]
+correct_count = 0
+
+for i in questions:
+    question_bank.append(question_model.Question(i["text"], i["answer"]))
+
+quiz = quiz_brain.QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
